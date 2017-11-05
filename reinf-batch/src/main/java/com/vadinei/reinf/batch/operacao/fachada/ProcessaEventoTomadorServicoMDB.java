9857e2c99a78838b6928520c177c554fa6e56f48
José Vádinei Soares <vadinei@hotmail.com>
2017-11-05 02:02:45 -0300

    Configurações básicas do JMS e criação do projeto reinf-deployer (EAR).

--- reinf-batch/src/main/java/com/vadinei/reinf/batch/operacao/fachada/ProcessaEventoTomadorServicoMDB.java
+++ reinf-batch/src/main/java/com/vadinei/reinf/batch/operacao/fachada/ProcessaEventoTomadorServicoMDB.java
@@ -1,18 +1,48 @@
 package com.vadinei.reinf.batch.operacao.fachada;
 
+import java.io.Serializable;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSession;
 
 /**
  * @author José Vádinei Soares - vadinei@hotmail.com
  *
  */
-@MessageDriven
-public class ProcessaEventoTomadorServicoMDB implements MessageListener {
+@MessageDriven(name = "ProcessaEventoTomadorServicoMDB", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms.queue.Test"),
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+public class ProcessaEventoTomadorServicoMDB implements MessageListener, Serializable {
+
+	@Resource
+	private Queue queue;
+
+	@Resource
+	private QueueConnectionFactory connectionFactory;
+
+	private QueueConnection connection;
+
+	private QueueSession session;
+
+	private MessageProducer messageProducer;
+
+	@EJB
+	private ProcessaEventoTomadorServicoSFLocal processaEventoTomadorServicoSF;
+
+	private static final long serialVersionUID = 745859427128618618L;
 
 	@Override
-	public void onMessage(final Message arg0) {
+	public void onMessage(final Message message) {
 
 	}
 
