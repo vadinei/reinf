42892e8580cc0f5b182d23a9b08fc989d031d857
José Vádinei Soares <vadinei@hotmail.com>
2017-11-08 18:43:40 -0300

    Criação: TOs ao R-2010 e do Template Method (ProcessaEventoASTemplate).

--- reinf-batch/src/main/java/com/vadinei/reinf/batch/operacao/to/EventoTomadorServicoInformacaoServicoTomadoTO.java
+++ reinf-batch/src/main/java/com/vadinei/reinf/batch/operacao/to/EventoTomadorServicoInformacaoServicoTomadoTO.java
@@ -13,4 +13,72 @@ public class EventoTomadorServicoInformacaoServicoTomadoTO implements Serializab
 	// ideEstabObra
 	private EventoTomadorServicoIdentificacaoEstabelecimentoObraTO identificacaoEstabelecimentoObraTO;
 
+	public EventoTomadorServicoInformacaoServicoTomadoTO() {
+		super();
+	}
+
+	/**
+	 * @return O atributo identificacaoEstabelecimentoObraTO
+	 */
+	public EventoTomadorServicoIdentificacaoEstabelecimentoObraTO getIdentificacaoEstabelecimentoObraTO() {
+		return identificacaoEstabelecimentoObraTO;
+	}
+
+	/**
+	 * @param identificacaoEstabelecimentoObraTO
+	 *            define o atributo identificacaoEstabelecimentoObraTO
+	 */
+	public void setIdentificacaoEstabelecimentoObraTO(
+			final EventoTomadorServicoIdentificacaoEstabelecimentoObraTO identificacaoEstabelecimentoObraTO) {
+		this.identificacaoEstabelecimentoObraTO = identificacaoEstabelecimentoObraTO;
+	}
+
+	/**
+	 * @return O atributo serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return EventoTomadorServicoInformacaoServicoTomadoTO.serialVersionUID;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result)
+				+ ((identificacaoEstabelecimentoObraTO == null) ? 0 : identificacaoEstabelecimentoObraTO.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final EventoTomadorServicoInformacaoServicoTomadoTO other = (EventoTomadorServicoInformacaoServicoTomadoTO) obj;
+		if (identificacaoEstabelecimentoObraTO == null) {
+			if (other.identificacaoEstabelecimentoObraTO != null) {
+				return false;
+			}
+		} else if (!identificacaoEstabelecimentoObraTO.equals(other.identificacaoEstabelecimentoObraTO)) {
+			return false;
+		}
+		return true;
+	}
+
 }
